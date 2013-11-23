dab74de031d684f72b9aba6a13b0243bf993982d
Jomelo <nilsfeld@gmail.com>
2013-11-23 12:36:47 +0100

    Update LCDMenuLib_makros.h

--- LCDMenuLib/LCDMenuLib_makros.h
+++ LCDMenuLib/LCDMenuLib_makros.h
@@ -3,8 +3,7 @@
 /*								LCDMenuLib								*/
 /*																		*/
 /************************************************************************/
-/* Autor:			Nils Feldkämper										*/
-/* Contact:			nilsfeld@gmail.com  (kein Support)					*/
+/* Autor:			Nils Feldkï¿½mper										*/
 /* Create:			03.02.2008											*/
 /* Edit:			22.11.2013											*/
 /************************************************************************/
@@ -17,10 +16,10 @@
 /*																		*/
 /************************************************************************/
 /* Deutsche Beschreibung:												*/
-/* Mit der Lib können LCD Menüs über mehrere Ebenen mit Hilfe des   	*/
-/* Nested Set Models generiert werden. Jeder Menüpunkt kann mit einer   */
+/* Mit der Lib kï¿½nnen LCD Menï¿½s ï¿½ber mehrere Ebenen mit Hilfe des   	*/
+/* Nested Set Models generiert werden. Jeder Menï¿½punkt kann mit einer   */
 /* Funktion hinterlegt werden die durch die Lib aufgerufen wird, sobald */
-/* der Menüpunkt aktiviert wird.										*/
+/* der Menï¿½punkt aktiviert wird.										*/
 /************************************************************************/
 /************************************************************************/
 
