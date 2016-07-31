deb691a2a228ec153e82b21d2f7042d6e4b59e15
Jomelo <nilsfeld@gmail.com>
2016-07-31 14:31:45 +0200

    change version

--- src/LCDMenuLib.h
+++ src/LCDMenuLib.h
@@ -16,7 +16,7 @@
 #ifndef LCDMenuLib_h
 #	define LCDMenuLib_h
 
-#	define _LCDML_VERSION							"LCDML v2.1.3"
+#	define _LCDML_VERSION							"LCDML v2.1.4"
 
 /* config */
 #	define _LCDML_DISP_cfg_cursor_deep				6		// save the last position of the cursor until layer xx
